761047d71123823b0e096847912f2448e7282a9c
twiby <t.balenbois@gmail.com>
2020-03-02 17:18:39 +0100

    Explain and better prepare dependencies

--- Resources/Scripts/install_linux_dependencies.sh
+++ Resources/Scripts/install_linux_dependencies.sh
@@ -5,8 +5,9 @@
 # sudo bash install_linux_dependencies.sh
 
 # install g++
-apt-get -y install build-essential
+apt-get -y install build-essential cmake g++-5
 
 # install Juce dependencies
 apt-get -y install freeglut3-dev libfreetype6-dev libxinerama-dev libxcursor-dev libasound2-dev libxrandr-dev
+
 
